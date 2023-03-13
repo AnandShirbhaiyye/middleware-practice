9a218c34b87ab1af1941245c4b31176b51a30f8d
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-03-13 21:59:15 +0530

    create sample api for middle ware practice

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+const TOKEN = "Anand@12345shir";
+
+const checkToken = (req, res, next) => {
+  const { token } = req.body;
+  if (token !== TOKEN) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized token",
+      data: null,
+    });
+  }
+  next();
+};
+let count = 0;
+
+const countApiCalls = (req, res, next) => {
+  count++;
+  console.log(`API calls: ${count}`);
+  next();
+};
+
+app.use(countApiCalls);
+
+app.post("/plant", checkToken, (req, res) => {
+  return res.json({
+    message: "create plant api",
+  });
+});
+
+app.post("/createplant", checkToken, (req, res) => {
+  res.json({
+    message: "Added plant successfully",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("port 5000");
+});
